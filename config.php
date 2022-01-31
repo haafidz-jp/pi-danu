c8270db6a62744bcd0ee7819ceca68eb842ee19f
haafidz-jp <haafidz123@gmail.com>
2022-01-31 23:04:11 +0700

    update config

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "localhost";
-$user = "haafidzx_danu";
-$password = "Power@123";
-$nama_database = "haafidzx_db_tournament";
+$server = "us-cdbr-east-05.cleardb.net";
+$user = "b53269878fd1ca";
+$password = "e9532d3b";
+$nama_database = "heroku_4c38b44740000e4";
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
 
